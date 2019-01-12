5bbabc43c82918030437e71a294f0425ee0e22db
Sacha <sacha.auday@hotmail.fr>
2019-01-12 19:53:55 +0200

    try pushing on keroku

--- Assignment/settings.py
+++ Assignment/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import django_heroku
+import dj_database_url
+from dj_database_url import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -74,17 +78,20 @@ WSGI_APPLICATION = 'Assignment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'mysql.connector.django',
-        'NAME': 'Billboard',
-        'USER': 'root',
-        'PASSWORD': 'rebecca09',
-        'HOST': 'localhost',
+        'NAME': 'team3db',
+        'USER': 'user_user',
+        'PASSWORD': 'user_user',
+        'HOST': 'db4free.net',
         'PORT': '3306',
     }
 }
 
+DATABASES['default'].update(dj_database_url.config(
+    default=config('DATABASE_URL'), conn_max_age=500))
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -109,20 +116,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'Asia/Jerusalem'
-
+TIME_ZONE = 'UTC'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/billboard/'
 
@@ -130,3 +139,5 @@ LOGIN_URL = '/login/'
 
 LOGOUT_REDIRECT_URL = '/login/'
 
+
+django_heroku.settings(locals())
